83bb1abbc8a8769c73034e7f757e31493af3f921
AksharaRamagiri <93607122+AksharaRamagiri@users.noreply.github.com>
2024-02-15 18:55:59 +0530

    Add files via upload

---
+++ Episode 01-Inception/Code/App.js
@@ -0,0 +1,32 @@
+
+    // const heading=React.createElement(
+    //     'h1',
+    //     {id:"heading"},
+    //     'Hello World From React !');
+    // console.log(heading); //OBJECT
+
+
+    // Nested Elements
+    
+    /*
+       <div id="parent">
+          <div id="child">
+            <h1>I am h1 Tag</h1>
+            <h2>I am h2 Tag</h2>
+           </div>
+        </div>
+    */
+    const parent=React.createElement("div",
+    {id:"parent"},
+    React.createElement("div",
+    {id:"child"},
+    [React.createElement("h1",
+    {},
+    "I'm h1 Tag."),React.createElement("h2",
+    {},
+    "I'm h2 Tag.")]));
+
+
+    console.log(parent);
+    const root=ReactDOM.createRoot(document.getElementById('root'));
+    root.render(parent);
